17c1557f85d279e60853b70f8414cb39cd1e8ee4
Ayesha Iqbal <sarajames0015@gmail.com>
2026-08-07 06:33:22 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int r=1, l=0, profit=0;
+        while(r<prices.length)
+        {
+            if(prices[l]<prices[r])
+            {
+                profit=Math.max(profit, prices[r]-prices[l]);
+            }
+            else
+            l=r;
+
+            r++;
+        }
+        return profit;
+    }
+}
